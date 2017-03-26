85dd9b055956d96eb633dc10957ba8adbce5b661
André Baart <andre@andrebaart.nl>
2017-03-26 22:26:02 +0200

    some admin page refinements for voice services and choice elements added some admin tweaks from the django tutorial

--- vsdk/service_development/admin.py
+++ vsdk/service_development/admin.py
@@ -1,9 +1,32 @@
 from django.contrib import admin
 from .models import VoiceService, MessagePresentation, Choice, ChoiceOption
 
+class VoiceServiceAdmin(admin.ModelAdmin):
+    fieldsets = [('General',    {'fields' : ['active', 'name', 'description']}),
+                    ('Call flow', {'fields': ['start_element']})]
+    list_display = ('name','active','is_valid_voice_service','modification_date','creation_date',)
+
+    def get_readonly_fields(self, request, obj=None):
+        """
+        Only allow activation of voice service if it is valid
+        """
+        if obj is not None:
+            if not obj.is_valid_voice_service():
+                return self.readonly_fields + ('active',)
+        return self.readonly_fields
+
+class ChoiceOptionsInline(admin.TabularInline):
+    model = ChoiceOption
+    extra = 2
+    fk_name = 'parent'
+
+class ChoiceAdmin(admin.ModelAdmin):
+    inlines = [ChoiceOptionsInline]
+
+
+
 # Register your models here.
 
-admin.site.register(VoiceService)
+admin.site.register(VoiceService, VoiceServiceAdmin)
 admin.site.register(MessagePresentation)
-admin.site.register(Choice)
-admin.site.register(ChoiceOption)
+admin.site.register(Choice, ChoiceAdmin)
--- vsdk/service_development/models.py
+++ vsdk/service_development/models.py
@@ -47,7 +47,7 @@ class Choice(VoiceServiceElement):
 class ChoiceOption(VoiceServiceElement): 
     parent = models.ForeignKey(
             Choice, 
-            #controlerne of dit wel echt cascade moet zijn???
+            #TODO: controlerne of dit wel echt cascade moet zijn???
             on_delete = models.CASCADE,
             related_name='%(app_label)s_%(class)s_parent_related')
     audio = models.ForeignKey(
@@ -58,7 +58,7 @@ class ChoiceOption(VoiceServiceElement):
             ) 
     redirect = models.ForeignKey(
             VoiceServiceElement, 
-            #controlerne of dit wel echt cascade moet zijn???
+            #TODO: controlerne of dit wel echt cascade moet zijn???
             on_delete = models.CASCADE,
             related_name='%(app_label)s_%(class)s_redirect_related')
 
@@ -73,10 +73,11 @@ class VoiceService(models.Model):
     description = models.CharField(max_length=10)
     creation_date = models.DateTimeField('date created', default=timezone.now)
     modification_date = models.DateTimeField('date last modified', default=timezone.now)
-    active = models.BooleanField('Activate this voice service')
+    active = models.BooleanField('Voice service active')
     start_element = models.ForeignKey(
             VoiceServiceElement,
             related_name='%(app_label)s_%(class)s_related')
+
 
     def __str__(self):
         return 'Voice Service: %s' % self.name
@@ -86,4 +87,9 @@ class VoiceService(models.Model):
         self.modification_date = timezone.now()
         return
 
+    def is_valid_voice_service(self):
+        #TODO: build voice service validator
+        return False
+    is_valid_voice_service.boolean = True
+    is_valid_voice_service.short_description = 'Valid'
 
