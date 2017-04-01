78dbaf36e818769bd983acfd9483a1b488cc6628
André Baart <andre@andrebaart.nl>
2017-04-01 13:11:52 +0200

    random progress 2

--- vsdk/voicelabels/admin.py
+++ vsdk/voicelabels/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-from .models import Language, VoiceLabel
+from .models import Language, VoiceLabel, VoiceFragment
+
+
+
+class VoiceLabelInline(admin.TabularInline):
+    model = VoiceFragment
+    extra = 2
+    fk_name = 'parent'
+
+class VoiceLabelAdmin(admin.ModelAdmin):
+    inlines = [VoiceLabelInline]
+
 
 # Register your models here.
 admin.site.register(Language)
-admin.site.register(VoiceLabel)
+admin.site.register(VoiceLabel, VoiceLabelAdmin)
--- vsdk/voicelabels/models.py
+++ vsdk/voicelabels/models.py
@@ -9,12 +9,30 @@ class Language(models.Model):
     def __str__(self):
         return '%s (%s)' % (self.name, self.code)
 
+
+
 class VoiceLabel(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "Voice Label: %s" % (self.name)
+
+    def is_valid(self):
+        return True
+
+    def validator(self):
+        return []
+
+
+class VoiceFragment(models.Model):
+    parent = models.ForeignKey(VoiceLabel,
+            on_delete = models.CASCADE)
     language = models.ForeignKey(
             Language,
             on_delete = models.CASCADE)
-    placeholderFileURL = models.CharField(max_length=50)
+    audio = models.FileField()
 
     def __str__(self):
-        return "(%s): %s" % (self.language.code , self.name)
+        return "Voice Fragment: (%s) %s" % (self.language.name, self.parent.name)
+
+
