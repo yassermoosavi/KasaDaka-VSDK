b298f106f5b471433bb0ec889b5c2e65767a37ef
André Baart <andre@andrebaart.nl>
2017-04-01 13:11:22 +0200

    random progress

--- vsdk/service_development/admin.py
+++ vsdk/service_development/admin.py
@@ -1,26 +1,50 @@
 from django.contrib import admin
 from .models import VoiceService, MessagePresentation, Choice, ChoiceOption
 
+
+
+
 class VoiceServiceAdmin(admin.ModelAdmin):
-    fieldsets = [('General',    {'fields' : ['active', 'name', 'description']}),
+    fieldsets = [('General',    {'fields' : ['active', 'is_valid', 'validation_details', 'name', 'description', 'supported_languages']}),
                     ('Call flow', {'fields': ['start_element']})]
-    list_display = ('name','active','is_valid_voice_service','modification_date','creation_date',)
+    list_display = ('name','active','is_valid')
+    readonly_fields = ('is_valid', 'validation_details')
 
     def get_readonly_fields(self, request, obj=None):
         """
         Only allow activation of voice service if it is valid
         """
         if obj is not None:
-            if not obj.is_valid_voice_service():
+            if not obj.is_valid():
                 return self.readonly_fields + ('active',)
         return self.readonly_fields
+
+    def validation_details(self, obj=None):
+        """
+        Creates a HTML list from all errors found in validation
+        """
+        return '<br/>'.join(obj.validator())
+    validation_details.allow_tags = True
+    
+
+class VoiceServiceElementAdmin(admin.ModelAdmin):
+    fieldsets = [('General',    {'fields' : [ 'is_valid', 'validation_details', 'name', 'description', 'voice_label']})]
+    list_display = ('name', 'service' ,'is_valid')
+    readonly_fields = ('is_valid', 'validation_details')
+     
+    def validation_details(self, obj=None):
+        """
+        Creates a HTML list from all errors found in validation
+        """
+        return '<br/>'.join(obj.validator())
+    validation_details.allow_tags = True
 
 class ChoiceOptionsInline(admin.TabularInline):
     model = ChoiceOption
     extra = 2
     fk_name = 'parent'
 
-class ChoiceAdmin(admin.ModelAdmin):
+class ChoiceAdmin(VoiceServiceElementAdmin):
     inlines = [ChoiceOptionsInline]
 
 
--- vsdk/service_development/models/voiceservice.py
+++ vsdk/service_development/models/voiceservice.py
@@ -9,7 +9,8 @@ import voicelabels
 class VoiceServiceElement(models.Model):
     creation_date = models.DateTimeField('date created', auto_now_add = True)
     modification_date = models.DateTimeField('date last modified', auto_now = True)
-    name = models.CharField(max_length=40)
+    name = models.CharField(max_length=100)
+    service = models.ForeignKey('VoiceService', on_delete = models.CASCADE)
     description = models.CharField(
             max_length = 500,
             blank = True)
@@ -25,6 +26,7 @@ class VoiceServiceElement(models.Model):
 
     def is_valid(self):
         return len(self.validator()) == 0
+    is_valid.boolean = True
 
     def validator(self):
         if self.voice_label:
@@ -51,6 +53,18 @@ class Choice(VoiceServiceElement):
     def __str__(self):
         return self.name
 
+    def is_valid(self):
+        return len(self.validator()) == 0
+    is_valid.boolean = True
+
+    def validator(self):
+        #TODO: check all children (choice options)
+        
+        if self.voice_label:
+            return self.voice_label.validator()
+        else:
+            return ['No VoiceLabel in element: "%s"'%self.name]
+
 class ChoiceOption(VoiceServiceElement):
     parent = models.ForeignKey(
             Choice,
@@ -61,11 +75,35 @@ class ChoiceOption(VoiceServiceElement):
             VoiceServiceElement, 
             #TODO: controlerne of dit wel echt cascade moet zijn???
             on_delete = models.CASCADE,
-            related_name='%(app_label)s_%(class)s_redirect_related')
+            related_name='%(app_label)s_%(class)s_redirect_related',
+            blank = True,
+            null = True)
 
     def __str__(self):
         return "(%s): %s" % (self.parent.name,self.name)
+    
+    def is_valid(self):
+        return len(self.validator()) == 0
+    is_valid.boolean = True
 
+    def validator(self):
+        errors = []
+        #check if voice label is present
+        if self.voice_label:
+            errors.extend(self.voice_label.validator())
+        else:
+            errors.append('No VoiceLabel in choice option: "%s"'%self.name)
+
+        #check if redirect is present
+        if not self.redirect:
+            errors.append('No redirect in choice option: "%s"'%self.name)
+        #check whether element that will be redirected to is appointed to the same voice service
+        elif not self.redirect in self.service.get_elements():
+            errors.append('Redirect "%s" in choice option "%s" does not belong to same voice service'% (self.redirect.name, self.name))
+
+        return errors
+
+ 
 class DataPresentation(VoiceServiceElement):
     pass
 
@@ -99,3 +137,6 @@ class VoiceService(models.Model):
         if len(self.supported_languages.all()) == 0:
             errors.append('No supported languages')
         return errors
+
+    def get_elements(self):
+        return []
