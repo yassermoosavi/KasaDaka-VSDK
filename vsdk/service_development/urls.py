8aad7fb62645ac9a2bff8e768403d32f030a5b24
André Baart <andre@andrebaart.nl>
2017-10-12 17:33:11 +0200

    intermediate commit

--- vsdk/service_development/urls.py
+++ vsdk/service_development/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     url(r'^start/(?P<voice_service_id>[0-9]+)$', views.voice_service_start, name='voice-service'),
     url(r'^start/(?P<voice_service_id>[0-9]+)/(?P<session_id>[0-9]+)$', views.voice_service_start, name='voice-service'),
     url(r'^user/register/$', views.user_registration, name = 'user-registration'),
-    url(r'^language_select/(?P<voice_service_id>[0-9]+)/(?P<session_id>[0-9]+)$', views.language_selection, name = 'language-selection'),
+    url(r'^language_select/(?P<voice_service_id>[0-9]+)/(?P<session_id>[0-9]+)$', views.LanguageSelection.as_view(), name = 'language-selection'),
 ]
 
--- vsdk/service_development/views/language.py
+++ vsdk/service_development/views/language.py
@@ -1,13 +1,40 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404, redirect
+from django.urls import reverse
+from django.views.generic import TemplateView
 
-from ..models import CallSession
+from ..models import CallSession, VoiceService, Language
 
-def language_selection(request, voice_service_id, session_id):
-    session = get_object_or_404(CallSession, pk = session_id)
+class LanguageSelection(TemplateView):
 
+    def get(self, request, voice_service_id, session_id):
+        """
+        Asks the user to select one of the supported languages.
+        """
+        session = get_object_or_404(CallSession, pk = session_id)
+        voice_service = get_object_or_404(VoiceService, pk = voice_service_id)
+        #get all supported languages
+        languages = session.service.supported_languages.all()
+        redirect_url = reverse('service-development:language-selection', args=[voice_service_id,session_id])
+        context = {'languages' : languages,
+                   'redirect_url' : redirect_url
+                   }
+        return render(request, 'language_selection.xml', context, content_type='text/xml')
 
-    #get all supported languages
-    languages = session.service.supported_languages.all()
-    context = {'languages' : languages,
-               'redirect_url' : 'bla'}
-    return render(request, 'user_registration.xml', context, content_type='text/xml')
+    def post(self, request, voice_service_id, session_id):
+        """
+        Saves the chosen language to the session
+        """
+        if 'language_id' not in request.POST:
+            #TODO return error
+            return 
+
+        session = get_object_or_404(CallSession, pk = session_id)
+        voice_service = get_object_or_404(VoiceService, pk = voice_service_id) 
+        language = get_object_or_404(Language, pk = request.POST['language_id'])
+
+        session._language = language
+        session.save()
+
+        return redirect('service-development:voice-service',
+                        voice_service_id = voice_service.id,
+                        session_id = session.id)
